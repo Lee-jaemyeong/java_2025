ed6856680b1bbd4f8e1e3e6fdb6b54e5698d550a
Lee-jaemyeong <kinglee98@naver.com>
2025-03-13 21:44:20 +0900

    2025-03-13 java_today_repeat

---
+++ java_basic/src/com/company/java018_ex/JavaIOEx004_shop.java
@@ -0,0 +1,36 @@
+package com.company.java018_ex;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class JavaIOEx004_shop {
+	public static void main(String[] args) {
+	 try {
+		String apiurl = "https://openapi.naver.com/v1/search/shop.json?query="
+				        + URLEncoder.encode("아우터","UTF-8");
+		URL url = new URL(apiurl);
+		
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("X-Naver-Client-Id", "k397eleMXGY25hqhpx5p");
+		conn.setRequestProperty("X-Naver-Client-Secret", "tAjvip6d9j");
+		
+		//System.out.println(conn.getResponseCode());
+		int code = conn.getResponseCode();
+		BufferedReader br;
+		if(code==200) {br = new BufferedReader(new InputStreamReader(conn.getInputStream()));}
+		else {br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));}
+		
+		String line=""; StringBuffer sb = new StringBuffer();
+		while((line=br.readLine()) != null) {sb.append(line + "\n");}
+		
+		System.out.println(sb.toString());
+		br.close(); conn.disconnect();
+		
+	 }catch(Exception e) {e.printStackTrace();}
+	}
+}
