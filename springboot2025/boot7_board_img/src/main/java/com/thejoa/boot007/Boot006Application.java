180576be62f0761f4e53a41c0cb0fd0f9b475605
Lee-jaemyeong <kinglee98@naver.com>
2025-04-16 18:24:42 +0900

    2025_04_16 spring_boot day_7

---
+++ springboot2025/boot7_board_img/src/main/java/com/thejoa/boot007/Boot006Application.java
@@ -0,0 +1,13 @@
+package com.thejoa.boot007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot006Application.class, args);
+	}
+
+}
